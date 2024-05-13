2c2788c4dc6c4824ad303d7684a1fffb95715648
SebCodeing <165015424+SebCodeing@users.noreply.github.com>
2024-05-13 21:10:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+//Global Variables
+var DIRECTION = {
+  IDLE: 0
+  UP:1
+  DOWN:2
+  LEFT:3
+  RIGHT:4
+};
+var rounds = [5, 5, 3, 2];
+var colours = []
 
+//The ball object (the cube that bounces back and fourth)
+var Ball = {
+  new: function (incrementedspeed) {
+    return {
+      width: 18,
+      height: 18,
+      x: (this.canvas.width / 2) -9,
+      y: (this.canvas.width / 2) -9,
