e11d95c40c05bb2a3ca742ba1b0c7de7761f5248
SebCodeing <165015424+SebCodeing@users.noreply.github.com>
2024-05-14 08:51:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var DIRECTION = {
   RIGHT:4
 };
 var rounds = [5, 5, 3, 2];
-var colours = []
+var colours = ['#1abc9c', '#2fe00b', '#1bb3ad', '#34986b','#983434'];
 
 //The ball object (the cube that bounces back and fourth)
 var Ball = {
@@ -17,3 +17,20 @@ var Ball = {
       height: 18,
       x: (this.canvas.width / 2) -9,
       y: (this.canvas.width / 2) -9,
+      moveX: DIRECTION.IDLE,
+      moveY: DIRECTION.IDLE,
+      speed: incrementedspeed || 7
+    };
+  }
+};
+
+//The ai object (The two lines that move up and down)
+var Ai = {
+  new: function (side) {
+    return {
+      width: 18,
+      height: 100,
+      x: side === 'left' ? 150 : this.canvas.width -150,
+      y: (this.canvas.height / 2) -35,
+      score 0,
+      
